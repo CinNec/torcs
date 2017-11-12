aca90ce04b0bb7bc69095e8dd534339e339c97e4
TijmenvanDijk <vandijktijmen@gmail.com>
2017-11-12 17:25:09 +0100

    Changed a line in Normalize so I could access the full data matrix

--- code/Normalize.py
+++ code/Normalize.py
@@ -1,53 +1,56 @@
 import csv
 from os import listdir
 
-
 class Normalize:
 
-    def __init__(self):
-        data = []
-        n = 0
-        while(n <= 20 ):
-            data.append([])
-            n += 1
-        i = 0
-        j = 0
-        files = [f for f in listdir("training_data")]
-        for file in files:
-            with open("training_data/" + file, newline='') as csvfile:
-                reader = csv.DictReader(csvfile,delimiter = ",")
-                for row in reader:
-                    data[0].append( float(row["SPEED"]))
-                    data[1].append( float(row["TRACK_POSITION"]))
-                    data[2].append( float(row["ANGLE_TO_TRACK_AXIS"]))
-                    data[3].append( float(row["TRACK_EDGE_0"]))
-                    data[4].append( float(row["TRACK_EDGE_1"]))
-                    data[5].append( float(row["TRACK_EDGE_2"]))
-                    data[6].append( float(row["TRACK_EDGE_3"]))
-                    data[7].append( float(row["TRACK_EDGE_4"]))
-                    data[8].append( float(row["TRACK_EDGE_5"]))
-                    data[9].append( float(row["TRACK_EDGE_6"]))
-                    data[10].append( float(row["TRACK_EDGE_7"]))
-                    data[11].append( float(row["TRACK_EDGE_8"]))
-                    data[12].append( float(row["TRACK_EDGE_9"]))
-                    data[13].append( float(row["TRACK_EDGE_10"]))
-                    data[14].append( float(row["TRACK_EDGE_11"]))
-                    data[15].append( float(row["TRACK_EDGE_12"]))
-                    data[16].append( float(row["TRACK_EDGE_13"]))
-                    data[17].append( float(row["TRACK_EDGE_14"]))
-                    data[18].append( float(row["TRACK_EDGE_15"]))
-                    data[19].append( float(row["TRACK_EDGE_16"]))
-                    data[20].append( float(row["TRACK_EDGE_17"]))
-                    
-        while (i < len(data)):
-            maxv = max(data[i])
-            minv = min(data[i])
-            j = 0
-            while(j < len(data[i])):
-                data[i][j] = (data[i][j] - minv)/(maxv-minv)
-                j += 1
-            i += 1
-        self.speed = data[0]
+	def __init__(self):
+		data = []
+		n = 0
+		while(n <= 20 ):
+			data.append([])
+			n += 1
+		i = 0
+		j = 0
+		files = [f for f in listdir("training_data")]
+		for file in files:
+			with open("training_data/" + file, newline='') as csvfile:
+				reader = csv.DictReader(csvfile,delimiter = ",")
+				for row in reader:
+					data[0].append( float(row["SPEED"]))
+					data[1].append( float(row["TRACK_POSITION"]))
+					data[2].append( float(row["ANGLE_TO_TRACK_AXIS"]))
+					data[3].append( float(row["TRACK_EDGE_0"]))
+					data[4].append( float(row["TRACK_EDGE_1"]))
+					data[5].append( float(row["TRACK_EDGE_2"]))
+					data[6].append( float(row["TRACK_EDGE_3"]))
+					data[7].append( float(row["TRACK_EDGE_4"]))
+					data[8].append( float(row["TRACK_EDGE_5"]))
+					data[9].append( float(row["TRACK_EDGE_6"]))
+					data[10].append( float(row["TRACK_EDGE_7"]))
+					data[11].append( float(row["TRACK_EDGE_8"]))
+					data[12].append( float(row["TRACK_EDGE_9"]))
+					data[13].append( float(row["TRACK_EDGE_10"]))
+					data[14].append( float(row["TRACK_EDGE_11"]))
+					data[15].append( float(row["TRACK_EDGE_12"]))
+					data[16].append( float(row["TRACK_EDGE_13"]))
+					data[17].append( float(row["TRACK_EDGE_14"]))
+					data[18].append( float(row["TRACK_EDGE_15"]))
+					data[19].append( float(row["TRACK_EDGE_16"]))
+					data[20].append( float(row["TRACK_EDGE_17"]))
+
+		while (i < len(data)):
+			maxv = max(data[i])
+			minv = min(data[i])
+			j = 0
+			while(j < len(data[i])):
+				data[i][j] = (data[i][j] - minv)/(maxv-minv)
+				j += 1
+			i += 1
+
+		# To get a full data matrix
+		self.data = data
+
+		self.speed = data[0]
 		self.track_position = data[1]
 		self.angle_to_track_axis = data[2]
 		self.track_edge_0 = data[3]
@@ -66,6 +69,5 @@ class Normalize:
 		self.track_edge_13 = data[16]
 		self.track_edge_14 = data[17]
 		self.track_edge_15 = data[18]
-		self.track_edge_16 = data[19]		
-		self.track_edge_16 = data[20]		
-		
+		self.track_edge_16 = data[19]
+		self.track_edge_16 = data[20]
--- code/feedfoward.py
+++ code/feedfoward.py
@@ -1,19 +1,92 @@
-import pickle
 import numpy as np
+from Normalize import Normalize
+
+# Build model creates a neural network with the specified number of hidden layers and neurons per layer
+
+# Create a weight matrix for a hidden layer 
+
+BIAS = 0
+
+class Layer:
+    def __init__(self, neurons, inputs_per_neuron):
+        # add 1 for bias 
+        self.weights = 2 * np.random.random((neurons, inputs_per_neuron)) - 1
+        self.bias = 2 * np.random.random((1, inputs_per_neuron)) - 1
+#        self.activation = 
+        
+class NeuralNetwork: 
+    def __init__(self):
+        self.layers = []
+        
+    def add_layer(self, layer):
+        self.layers.append(layer)
+    
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
+    
+    # Calculate the output using the weights
+    def forward_propagation(self, input):
+        for layer in self.layers:
+            print("input:", input)
+            print("layer.weights:", layer.weights)
+#            input = self.sigmoid(input.dot(layer.weights) + layer.bias * BIAS)
+            input = self.sigmoid(input.dot(layer.weights))
+            print("input after dot:", input)
+        output = input
+        return output
+    
+    # Calculate the error using cross-entropy error
+    def error(self, t, y):
+        return np.sum(np.square(t - y))
+        return - np.sum(np.multiply(t, np.log(y)) + np.multiply((1-t), np.log(1-y)))
+    
+    # The derivative of the error function
+    def derivative_error(self, t, y):
+        return y - t
+        
+    # Propagate error backwards to find gradients
+    def backward_propagation(self, t, y):
+        # Find gradients for output layer weights
+        gradient_output = derivative_error(y - t)
+        layer_delta = gradient_output * self.sigmoid_derivative(y)
+        
+        for hidden_layer in self.layers[0:-1]
+            layer_error = layer_delta.dot(hidden_layer.weights)
+        return
+    
+    # Update the weights using batch SGD
+    
+
+# decides on an activation function
+#def activation(type)
+#    if type == sigmoid:
+#        return 
+    
+layer1 = Layer(2, 2)
+layer2 = Layer(1, 2)
+
+layer1.weights = np.array([[0.4, 0.2], [0.65, 0.1]])
+layer2.weights = np.array([0.8, 0.15])
+
+nn = NeuralNetwork()
+nn.add_layer(layer1)
+nn.add_layer(layer2)
+
+x = np.array([0.1, 0.35])
 
 
-#a = open("drivelog.pickle",'r', encoding="utf-8")
-#log = pickle.load(a)
-#print(log)
+y = nn.forward_propagation(x)
+print("y:", y)
 
-with open("drivelog.pickle", 'rb') as logfile:
-    a = pickle.Unpickler(logfile)
-    print(type(a))
-    print(a)
-    
-def forward():
-    return
-    
-    
-def backward():
-    return
+t = np.array([0.9, 0.35])
+y = np.array([0.8, 0.5])
+error = nn.error(t, y)
+print("error:", error)
+
+# data processing
+#data = np.array(Normalize().data)
+
+
