c56a5ef77834e20cd2b12612069a3ea790b2ab6a
Marcostantini <marcostantini2008@gmail.com>
2017-11-14 12:49:25 +0100

    debug

--- my_driver.py
+++ my_driver.py
@@ -5,13 +5,19 @@ from pytocl.car import State, Command
 class MyDriver(Driver):
     # Override the `drive` method to create your own driver
     ...
-    print (list(carstate.distances_from_edge))
+    # print (list(carstate.distances_from_edge))
     # def drive(self, carstate: State) -> Command:
-    	# nn_input = 
-    	# [carstate.speed_x,
-
-
-    	# ]
     #     command = Command(...)
+    	# nn_input = [
+    	# command.accelerator,
+    	# command.brake,
+    	# command.steering,
+    	# carstate.speed_x,
+    	# carstate.distance_from_center,
+    	# carstate.accelerator,
+    	# carstate.accelerator,
+    	# carstate.accelerator,
+    	# carstate.accelerator
+    	# ]
     #     return command
     	
--- pytocl/car.py
+++ pytocl/car.py
@@ -67,8 +67,6 @@ class State(Value):
 
     def __init__(self, sensor_dict):
         """Creates decoded car state from sensor value dictionary."""
-        # print ()
-        # print (self.floats_value(sensor_dict, 'track'))
         self.angle = self.float_value(sensor_dict, 'angle') * \
             DEGREE_PER_RADIANS
         self.current_lap_time = self.float_value(sensor_dict, 'curLapTime')
@@ -101,6 +99,7 @@ class State(Value):
             sensor_dict,
             'focus'
         )
+        print (self.distance_from_center)
 
     @property
     def distances_from_egde_valid(self):
