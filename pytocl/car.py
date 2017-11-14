24c89017d9678b8d67bbd5a07dc6500761412cbc
Marcostantini <marcostantini2008@gmail.com>
2017-11-14 11:35:21 +0100

    debug

--- pytocl/car.py
+++ pytocl/car.py
@@ -67,7 +67,7 @@ class State(Value):
 
     def __init__(self, sensor_dict):
         """Creates decoded car state from sensor value dictionary."""
-        print floats_value(sensor_dict, 'track')
+        print (self.floats_value(sensor_dict, 'track'))
         self.angle = self.float_value(sensor_dict, 'angle') * \
             DEGREE_PER_RADIANS
         self.current_lap_time = self.float_value(sensor_dict, 'curLapTime')
