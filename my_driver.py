dab81855ac48014b7e74826437341cab8a419139
Marcostantini <marcostantini2008@gmail.com>
2017-11-14 12:26:30 +0100

    debug

--- my_driver.py
+++ my_driver.py
@@ -5,10 +5,11 @@ from pytocl.car import State, Command
 class MyDriver(Driver):
     # Override the `drive` method to create your own driver
     ...
+    print (list(carstate.distances_from_edge))
     # def drive(self, carstate: State) -> Command:
     	# nn_input = 
     	# [carstate.speed_x,
-    	
+
 
     	# ]
     #     command = Command(...)
