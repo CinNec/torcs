2cc24f8c9caba1a1c55bda32141b63d15a369fe8
Marcostantini <marcostantini2008@gmail.com>
2017-11-16 12:51:20 +0100

    changed indentation

--- my_driver.py
+++ my_driver.py
@@ -8,12 +8,11 @@ with open("pickled_nn.txt", "rb") as pickle_file:
 
 class MyDriver(Driver):
     # Override the `drive` method to create your own driver
-    ...
     def drive(self, carstate: State) -> Command:
         command = Command()
-    	nn_input = [carstate.speed_x, carstate.distance_from_center, carstate.angle] + list(carstate.distances_from_edge)
+        nn_input = [carstate.speed_x, carstate.distance_from_center, carstate.angle]+list(carstate.distances_from_edge)
         nn_output = nn.forward_propagation(nn_input)
-    	command.acceleration = nn_output[0]
-    	command.brake = nn_output[1]
-    	command.steering = nn_output[2]
+        command.acceleration = nn_output[0]
+        command.brake = nn_output[1]
+        command.steering = nn_output[2]
         return command
