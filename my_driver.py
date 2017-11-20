aee072b1a1477c2521a7503406f15ae63dacf64f
Marcostantini <marcostantini2008@gmail.com>
2017-11-20 15:29:17 +0100

    out of track recovery test

--- my_driver.py
+++ my_driver.py
@@ -15,29 +15,32 @@ from feedforward import Ndata
 with open("pickled_nn.txt", "rb") as pickle_file:
     nn = pickle.load(pickle_file)
 
+last_dfc = 0
+
 class MyDriver(Driver):
+    self.last_dfc = 0
     # Override the `drive` method to create your own driver
     def drive(self, carstate: State) -> Command:
         command = Command()
-        # nn_input = np.array([carstate.speed_x, carstate.distance_from_center, carstate.angle]+list(carstate.distances_from_edge)[0:-1])
-        # i=0
-        # while(i <= 20):
-        #     nn_input[i] = nn_input[i] = (nn_input[i] - Ndata.minarray[i])/(Ndata.maxarray[i]-Ndata.minarray[i])
-        #     i += 1
-        # nn_output = nn.forward_propagation(nn_input)
+        nn_input = np.array([carstate.speed_x, carstate.distance_from_center, carstate.angle]+list(carstate.distances_from_edge)[0:-1])
+        i=0
+        while(i <= 20):
+            nn_input[i] = nn_input[i] = (nn_input[i] - Ndata.minarray[i])/(Ndata.maxarray[i]-Ndata.minarray[i])
+            i += 1
+        nn_output = nn.forward_propagation(nn_input)
 
-        # command.accelerator= round(nn_output[0])
-        # command.brake = round(nn_output[1])
-        # command.steering = nn_output[2]
+        command.accelerator= round(nn_output[0])
+        command.brake = round(nn_output[1])
+        command.steering = nn_output[2]
 
-        # acceleration = command.accelerator
+        acceleration = command.accelerator
 
-        # if acceleration > 0:
-        #     if abs(carstate.distance_from_center) >= 1:
-        #         # off track, reduced grip:
-        #         acceleration = min(0.2, acceleration)
+        if acceleration > 0:
+            if abs(carstate.distance_from_center) >= 1:
+                # off track, reduced grip:
+                acceleration = min(0.2, acceleration)
 
-        #     command.accelerator = min(acceleration, 1)
+            command.accelerator = min(acceleration, 1)
 
         if carstate.rpm > 8000:
             command.gear = carstate.gear + 1
@@ -48,8 +51,13 @@ class MyDriver(Driver):
 
         if not command.gear:
             command.gear = carstate.gear or 1
-        command.accelerator = 1
-        command.brake = 0
-        command.steering = -1000000000000
+
+        if abs(carstate.distance_from_center) >= 1:
+            command.brake = 0
+            if abs(carstate.distance_from_center) >= abs(self.last_dfc)
+                command.steering = -1
+            else 
+                command.steering = 0
+        self.last_dfc = carstate.distance_from_center
 
         return command
