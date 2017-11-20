c70793c25abd375df71d29fcc76d34b5b4ada484
Marcostantini <marcostantini2008@gmail.com>
2017-11-20 15:15:06 +0100

    hd5

--- my_driver.py
+++ my_driver.py
@@ -50,6 +50,6 @@ class MyDriver(Driver):
             command.gear = carstate.gear or 1
         command.accelerator = 1
         command.brake = 0
-        command.steering = 1
+        command.steering = -1
 
         return command
