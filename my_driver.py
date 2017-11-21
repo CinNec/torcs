6f6d624a792ded9ca660b4e565cc00ebf5bbd61a
Marcostantini <marcostantini2008@gmail.com>
2017-11-21 13:56:51 +0100

    only print

--- my_driver.py
+++ my_driver.py
@@ -38,9 +38,9 @@ class MyDriver(Driver):
         
         mlp_output = mlp.predict([nn_input])
         print (mlp_output)
-        command.accelerator= round(mlp_output[0])
-        command.brake = round(mlp_output[1])
-        command.steering = mlp_output[2]
+        # command.accelerator= round(mlp_output[0])
+        # command.brake = round(mlp_output[1])
+        # command.steering = mlp_output[2]
 
 
         # GEAR HANDLER
