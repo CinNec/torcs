b9dbff354d8a0707d91d6f7a74fc1187f18785f5
Marcostantini <marcostantini2008@gmail.com>
2017-11-28 14:48:08 +0100

    h

--- ea.py
+++ ea.py
@@ -13,9 +13,9 @@ class EvoAlg():
         if carstate[0] > 0.2 and carstate[3] < 1:
             command.append(1)
         else: command.append(0)
-        if (carstate[1] < -0.2 or carstate[2] > 0.2) and carstate[4] < 1:
+        if (carstate[1] < -0.2 or (carstate[2] > 0.2 and carstate[1] <= 0.2)) and carstate[4] < 1:
             steering = carstate[4] + 0.1
-        if (carstate[1] > 0.2 or carstate[2] < -0.2) and carstate[4] > -1:
+        if (carstate[1] > 0.2 or (carstate[2] < -0.2 and carstate[1] >= -0.2)) and carstate[4] > -1:
             steering = carstate[4] - 0.1
         command.append(steering)
         return command
