34b57ceb1ada914b0291e1908d11fb9d200dbdff
Marcostantini <marcostantini2008@gmail.com>
2017-11-28 14:35:27 +0100

    c

--- ea.py
+++ ea.py
@@ -7,7 +7,7 @@ class EvoAlg():
         command = []
         if carstate[3] < 0:
             carstate[3] = 1
-        if carstate[0] < 0.2 or carstate[3] = 1:
+        if carstate[0] < 0.2 or carstate[3] == 1:
             command.append(1)
         else: command.append(0)
         if carstate[0] > 0.2 and carstate[3] < 1:
