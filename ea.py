12ea6a2fe6517c3700e3cfbdbed8c36307932a10
Marcostantini <marcostantini2008@gmail.com>
2017-11-28 17:56:53 +0100

    improved hardcoding

--- ea.py
+++ ea.py
@@ -5,6 +5,7 @@ class EvoAlg():
     def ea_output(self, carstate):
         steering = 0
         command = []
+        c_dist = 0.3
         if carstate[3] < 0:
             carstate[3] = 1
         if carstate[0] < 0.2 or carstate[3] == 1:
@@ -13,9 +14,9 @@ class EvoAlg():
         if carstate[0] > 0.2 and carstate[3] < 1:
             command.append(1)
         else: command.append(0)
-        if (carstate[1] < -0.40 or carstate[2] > 0.35) and carstate[4] < 1:
-            steering = carstate[4] + 0.005
-        if (carstate[1] > 0.40 or carstate[2] < -0.35) and carstate[4] > -1:
-            steering = carstate[4] - 0.005
+        if (carstate[1] < -c_dist or (carstate[2] > 0.35 and carstate[1] <= c_dist)) and carstate[4] < 1:
+            steering = carstate[4] + 0.01
+        if (carstate[1] > c_dist or (carstate[2] < -0.35 and carstate[1] >= -c_dist)) and carstate[4] > -1:
+            steering = carstate[4] - 0.01
         command.append(steering)
         return command
