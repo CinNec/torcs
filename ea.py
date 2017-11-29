6ff13c1993c6357ca38ed1cdb862da0cf2e3d014
Marcostantini <marcostantini2008@gmail.com>
2017-11-29 17:10:50 +0100

    elif

--- ea.py
+++ ea.py
@@ -27,7 +27,7 @@ class EvoAlg():
             steering = carstate[4] + 0.005
             command[0] = 0.5
         # if (carstate[1] > c_dist or (carstate[2] < -0.35 and carstate[1] >= -c_dist and carstate[4] <= 0)) and carstate[4] > -1:
-        if (carstate[1] > c_dist or carstate[2] < -0.35) and carstate[4] > -1:
+        elif (carstate[1] > c_dist or carstate[2] < -0.35) and carstate[4] > -1:
             steering = carstate[4] - 0.005
             command[0] = 0.5
         command.append(steering)
