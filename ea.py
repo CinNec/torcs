91869ede3764f696561a255518ece632ad1b1ad9
Marcostantini <marcostantini2008@gmail.com>
2017-11-29 18:59:47 +0100

    a

--- ea.py
+++ ea.py
@@ -8,7 +8,8 @@ class EvoAlg():
         steering = 0
         command = []
         c_dist = 0.6
-        # carstate[2] = carstate[2] / float(180)
+        angle = 0.1
+        carstate[2] = carstate[2] / float(180)
         print (carstate[0])
         print (carstate[1])
         print (carstate[2])
@@ -23,11 +24,11 @@ class EvoAlg():
         # if carstate[3] < 1:
             command.append(1)
         else: command.append(0)
-        if (carstate[1] < -c_dist or (carstate[2] > 0.35 and carstate[1] <= c_dist and carstate[4] >= 0)) and carstate[4] < 1:
+        if (carstate[1] < -c_dist or (carstate[2] > angle and carstate[1] <= c_dist and carstate[4] >= 0)) and carstate[4] < 1:
         # if (carstate[1] < -c_dist or carstate[2] > 0.05) and carstate[4] < 1:
             steering = carstate[4] + 0.008
             command[0] = 0.5
-        if (carstate[1] > c_dist or (carstate[2] < -0.35 and carstate[1] >= -c_dist and carstate[4] <= 0)) and carstate[4] > -1:
+        elif (carstate[1] > c_dist or (carstate[2] < -angle and carstate[1] >= -c_dist and carstate[4] <= 0)) and carstate[4] > -1:
         # elif (carstate[1] > c_dist or carstate[2] < -0.05) and carstate[4] > -1:
             steering = carstate[4] - 0.008
             command[0] = 0.5
