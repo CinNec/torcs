e41738123b513752d8e6aebf2242106ee2b38c06
Marcostantini <marcostantini2008@gmail.com>
2017-11-29 19:46:53 +0100

    printing command

--- ea.py
+++ ea.py
@@ -13,13 +13,14 @@ class EvoAlg():
         # print (carstate[0])
         # print (carstate[1])
         # print (carstate[2])
-        print (carstate[3])
+        # print (carstate[3])
         # print (carstate[4])
-        if carstate[3] < 0:
+        if carstate[3] == 0:
             carstate[3] = 1
         if carstate[0] < 0.2 or carstate[3] == 1:
             command.append(0.8)
-        else: command.append(0)
+        else: 
+            command.append(0)
         if carstate[0] > 0.2 and carstate[3] < 1:
         # if carstate[3] < 1:
             command.append(1)
@@ -43,4 +44,5 @@ class EvoAlg():
                 carstate[4] = 0
             steering = carstate[4] - 0.008
         command.append(steering)
+        print (command)
         return command
