031d2a33ec0bac80ffbf35a2d9a6d946184413b0
Marcostantini <marcostantini2008@gmail.com>
2017-11-29 17:02:15 +0100

    normal brk

--- ea.py
+++ ea.py
@@ -18,8 +18,8 @@ class EvoAlg():
         if carstate[0] < 0.2 or carstate[3] == 1:
             command.append(0.8)
         else: command.append(0)
-        # if carstate[0] > 0.2 and carstate[3] < 1:
-        if carstate[3] < 1:
+        if carstate[0] > 0.2 and carstate[3] < 1:
+        # if carstate[3] < 1:
             command.append(1)
         else: command.append(0)
         if (carstate[1] < -c_dist or (carstate[2] > 0.35 and carstate[1] <= c_dist and carstate[4] >= 0)) and carstate[4] < 1:
