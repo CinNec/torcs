8e7dfd6019b4fec24dd592229fa6643753c5d344
Marcostantini <marcostantini2008@gmail.com>
2017-11-29 20:06:15 +0100

    a

--- my_driver.py
+++ my_driver.py
@@ -30,7 +30,7 @@ class MyDriver(Driver):
         self.steering = 0
         self.stuck_step = 0
         self.stuck_counter = 0
-        self.stuck_recovery = 100
+        self.stuck_recovery = 200
         self.stuck_period = 250
         self.stuck = False
 
